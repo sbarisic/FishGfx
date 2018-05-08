dee8c18105ab680c4f20f8b7be1872eaa3d80898
Saša Barišić <cartman300@net.hr>
2018-05-08 19:33:36 +0200

    Vertex3, Mesh3D

---
+++ FishGfx/Vertex3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace FishGfx {
+	public struct Vertex3 {
+		public Vector3 Position;
+		public Vector2 UV;
+		public Color Color;
+
+		public Vertex3(Vector3 Pos) {
+			Position = Pos;
+			UV = new Vector2(0, 0);
+			Color = Color.White;
+		}
+
+		public static implicit operator Vertex3(Vector3 Pos) {
+			return new Vertex3(Pos);
+		}
+	}
+}
