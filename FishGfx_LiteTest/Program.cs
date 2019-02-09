a56cfde6d302917000a91a9d713db3d01c1c3694
Saša Barišić <cartman300@net.hr>
2019-02-09 17:47:25 +0100

    Latest

--- FishGfx_LiteTest/Program.cs
+++ FishGfx_LiteTest/Program.cs
@@ -1,8 +1,10 @@
 ﻿using FishGfx;
+using FishGfx.Formats;
 using FishGfx.Graphics;
 using FishGfx.Graphics.Drawables;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Numerics;
 using System.Text;
@@ -11,11 +13,31 @@ using System.Threading.Tasks;
 namespace FishGfx_LiteTest {
 	class Program {
 		static void Main(string[] args) {
-			Vector2 WindowSize = RenderWindow.GetDesktopResolution() * 0.9f;
+			Vector2 WindowSize = RenderWindow.GetDesktopResolution() * 0.5f;
 			RenderWindow RWind = new RenderWindow((int)WindowSize.X, (int)WindowSize.Y, "Test Lite");
-			
+
+			Mesh3D CubeMesh = new Mesh3D();
+			CubeMesh.SetVertices(Obj.Load("cube.obj").First().Vertices.ToArray());
+
+			ShaderProgram Shader = new ShaderProgram(new ShaderStage(ShaderType.VertexShader, "data/shaders/default3d.vert"),
+				new ShaderStage(ShaderType.FragmentShader, "data/shaders/default_tex_clr.frag"));
+
+			ShaderUniforms.Current.Camera.SetPerspective(WindowSize);
+			ShaderUniforms.Current.Camera.Position = new Vector3(1.5f);
+			ShaderUniforms.Current.Camera.LookAt(Vector3.Zero);
+
+			Texture Tex = Texture.FromFile("test.png");
+			Stopwatch SWatch = Stopwatch.StartNew();
+
 			while (!RWind.ShouldClose) {
 				Gfx.Clear();
+				ShaderUniforms.Current.Model = Matrix4x4.CreateFromYawPitchRoll(SWatch.ElapsedMilliseconds / 1000.0f, 0, 0);
+
+				Tex.BindTextureUnit();
+				Shader.Bind(ShaderUniforms.Current);
+				CubeMesh.Draw();
+				Shader.Unbind();
+				Tex.UnbindTextureUnit();
 
 				RWind.SwapBuffers();
 				Events.Poll();
